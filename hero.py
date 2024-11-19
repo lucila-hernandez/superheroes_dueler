39111ab6cc7eb372731d9cce14d2c2c836cbd4ba
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2024-11-18 17:48:25 -0800

    implemented team attack and defence

--- hero.py
+++ hero.py
@@ -1,6 +1,7 @@
 from ability import Ability
 from armor import Armor
 from weapon import Weapon
+from team import Team
 import random
 
 class Hero:
@@ -10,15 +11,35 @@ class Hero:
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+        self.deaths = 0
+        self.kills = 0
 
     def fight(self, opponent):
-        """Current Hero Will take turn fighting the opponent hero passed in."""
-        winner = random.choice([self, opponent])
-        if winner == self:
-            print(f"{self.name} defeats {opponent.name}!")
-        else:
-            print(f"{opponent.name} defeats {self.name}")
+        ''' Current Hero will take turns fighting the opponent hero passed in.
+        '''
+        if len(self.abilities) == 0 and len(opponent.abilities) == 0:
+            print("Draw")
+            return
+                
+        while self.is_alive() and opponent.is_alive():
+            damage_to_opponent = self.attack()
+            opponent.take_damage(damage_to_opponent)
 
+            if not opponent.is_alive():
+                print(f"{self.name} won!")
+                self.add_kill(1)
+                opponent.add_death(1)
+                return
+            
+            damage_to_hero = opponent.attack()
+            self.take_damage(damage_to_hero)
+
+            if not self.is_alive():
+                print(f"{opponent.name} won!")
+                opponent.add_kill(1)
+                self.add_death(1)
+                return
+                
     def add_ability(self, ability):
         ''' Add ability to abilities list '''
         self.abilities.append(ability)
@@ -55,42 +76,40 @@ class Hero:
         defense = self.defend()
         damage_taken = max(0, damage - defense)
         self.current_health -= damage_taken
-        print(f"{self.name} takes {damage_taken} damage, current health is now {self.current_health}.")
 
     def is_alive(self):
         '''Return True or False depending on whether the hero is alive or not.
         '''
         return self.current_health > 0
     
-    def fight(self, opponent):
-        ''' Current Hero will take turns fighting the opponent hero passed in.
-        '''
-        if len(self.abilities) == 0 and len(opponent.abilities) == 0:
-            print("Draw")
-            return
-        while self.is_alive() and opponent.is_alive():
-            damage_to_opponent = self.attack()
-            opponent.take_damage(damage_to_opponent)
-
-            if not opponent.is_alive():
-                print(f"{self.name} won!")
-                return
-            
-            damage_to_hero = opponent.attack()
-            self.take_damage(damage_to_hero)
-
-            if not self.is_alive():
-                print(f"{opponent.name} won!")
-                return
-    
     def add_weapon(self, weapon):
         """Add weapon to self.abilities"""
         self.abilities.append(weapon)
-            
+    
+    def add_kill(self, num_kills):
+        '''Update self.kills by num_kills amount'''
+        self.kills += num_kills
+    
+    def add_death(self,num_deaths):
+        '''Update deaths with num_deaths'''
+        self.deaths += num_deaths
+
+
         
 
+
+
+
+        
 if __name__ == "__main__":
-    hero = Hero("Wonder Woman")
-    weapon = Weapon("Lasso of Truth", 90)
-    hero.add_weapon(weapon)
-    print(hero.attack())
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
---
+++ team.py
@@ -0,0 +1,41 @@
+import random
+
+class Team:
+    def __init__(self,name):
+        self.name = name
+        self.heroes = []
+
+    def add_hero(self, hero):
+        '''Add hero to the team'''
+        self.heroes.append(hero)
+
+    def stats(self):
+        '''Print team statistics''' 
+        for hero in self.heroes:
+            kd = hero.kills / hero.deaths
+            print(f"{hero.name} Kill/Deaths: {kd}")
+    
+    def revive_heroes(self, health=100):
+        '''Reset all heroes health to starting_health'''
+        for hero in self.heroes:
+            hero.current_health = hero.starting_health
+    
+    def attack(self, other_team):
+        '''Battle each team against each other.'''
+        living_heroes = list()
+        living_opponents = list()
+
+        for hero in self.heroes: 
+            living_heroes.append(hero)
+        
+        for hero in other_team.heroes:
+            living_opponents.append(hero)
+        
+        while len(living_heroes) > 0 and len(living_opponents)> 0:
+            hero1 = random.choice(living_heroes)
+            hero2 = random.choice(living_opponents)
+
+            hero1.fight(hero2)
+
+            living_heroes = [hero for hero in self.heroes if hero.is_alive()]
+            living_opponents = [hero for hero in other_team.heroes if hero.is_alive()]
